2e8b501048398319795f0f5928b673e3e765e6e6
joshmotley <joshkmotley@gmail.com>
2015-08-23 16:20:59 -0400

    evenly spaced card views project

--- OOP-Cards-Views/CardGridViewController.m
+++ OOP-Cards-Views/CardGridViewController.m
@@ -30,6 +30,30 @@
     
     UIImageView *card1 = self.cardsCollection[0];
     UIImageView *card2 = self.cardsCollection[1];
+    UIImageView *card3 = self.cardsCollection[2];
+    UIImageView *card4 = self.cardsCollection[3];
+    UIImageView *card5 = self.cardsCollection[4];
+    UIImageView *card6 = self.cardsCollection[5];
+    UIImageView *card7 = self.cardsCollection[6];
+    UIImageView *card8 = self.cardsCollection[7];
+    UIImageView *card9 = self.cardsCollection[8];
+    UIImageView *card10 = self.cardsCollection[9];
+    UIImageView *card11 = self.cardsCollection[10];
+    UIImageView *card12 = self.cardsCollection[11];
+    
+    
+    card1.frame = CGRectMake(20, 0, 80, 122);
+    card2.frame = CGRectMake(120, 0, 80, 122);
+    card3.frame = CGRectMake(220, 0, 80, 122);
+    card4.frame = CGRectMake(20, 142, 80, 122);
+    card5.frame = CGRectMake(120, 142, 80, 122);
+    card6.frame = CGRectMake(220, 142, 80, 122);
+    card7.frame = CGRectMake(20, 284, 80, 122);
+    card8.frame = CGRectMake(120, 284, 80, 122);
+    card9.frame = CGRectMake(220, 284, 80, 122);
+    card10.frame = CGRectMake(20, 426, 80, 122);
+    card11.frame = CGRectMake(120, 426, 80, 122);
+    card12.frame = CGRectMake(220, 426, 80, 122);
     
 }
 
